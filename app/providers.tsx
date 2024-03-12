3e2c437efd920702edb44662cc80ee641e595149
Goraniya Karan <karangoraniya@gmail.com>
2024-03-12 18:46:33 +0530

    feat: sui provider config updated 🚀

--- app/providers.tsx
+++ app/providers.tsx
@@ -9,7 +9,6 @@ import { getFullnodeUrl, type SuiClientOptions } from '@mysten/sui.js/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '@mysten/dapp-kit/dist/index.css';
 
-// Config options for the networks you want to connect to
 const { networkConfig } = createNetworkConfig({
   testnet: { url: getFullnodeUrl('testnet') },
   mainnet: { url: getFullnodeUrl('mainnet') },
